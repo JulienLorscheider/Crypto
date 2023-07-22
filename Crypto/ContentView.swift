f759eb42e2c48128e0e2aef4a3c8dcd37d0be1b2
LORSCHEIDER Julien <julien.lorscheider8@etu.univ-lorraine.fr>
2023-07-22 22:25:29 +0200

    Initial Commit

---
+++ Crypto/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Crypto
+//
+//  Created by Julien on 22/07/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Crypto/CryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoApp.swift
+//  Crypto
+//
+//  Created by Julien on 22/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct CryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
